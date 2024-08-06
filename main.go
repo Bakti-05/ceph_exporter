44f320558f1a6230f2d7ac5721d7f9965088ac02
bakti balamaru <bakti.balamaru@datacomm.co.id>
2024-08-06 11:36:21 +0700

    update exporter

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ var (
 			Name: "pvc_disk_usage_bytes",
 			Help: "Disk usage of the PVC directory",
 		},
-		[]string{"directory"},
+		[]string{"path"},
 	)
 )
 
@@ -28,11 +28,11 @@ func init() {
 	prometheus.MustRegister(pvcUsage)
 }
 
-func measureDiskUsage(directory string) float64 {
-	cmd := exec.Command("du", "-sb", directory)
+func measureDiskUsage(path string) float64 {
+	cmd := exec.Command("du", "-sb", path)
 	out, err := cmd.Output()
 	if err != nil {
-		log.Printf("Error running du command: %v", err)
+		log.Printf("Error running du command on %s: %v", path, err)
 		return 0
 	}
 
@@ -43,7 +43,24 @@ func measureDiskUsage(directory string) float64 {
 
 	usage, err := strconv.ParseFloat(output[0], 64)
 	if err != nil {
-		log.Printf("Error parsing output: %v", err)
+		log.Printf("Error parsing output for %s: %v", path, err)
+		return 0
+	}
+
+	return usage
+}
+
+func measureFileUsage(path string) float64 {
+	cmd := exec.Command("stat", "-c", "%s", path)
+	out, err := cmd.Output()
+	if err != nil {
+		log.Printf("Error running stat command on %s: %v", path, err)
+		return 0
+	}
+
+	usage, err := strconv.ParseFloat(strings.TrimSpace(string(out)), 64)
+	if err != nil {
+		log.Printf("Error parsing output for %s: %v", path, err)
 		return 0
 	}
 
@@ -53,23 +70,34 @@ func measureDiskUsage(directory string) float64 {
 func recordMetrics(baseDir string) {
 	go func() {
 		for {
+			totalUsage := 0.0
+
 			err := filepath.Walk(baseDir, func(path string, info os.FileInfo, err error) error {
 				if err != nil {
 					log.Printf("Error walking through path %s: %v", path, err)
 					return err
 				}
 
+				var usage float64
 				if info.IsDir() {
-					usage := measureDiskUsage(path)
-					pvcUsage.WithLabelValues(path).Set(usage)
-					log.Printf("Directory: %s, Usage: %f bytes", path, usage)
+					usage = measureDiskUsage(path)
+				} else {
+					usage = measureFileUsage(path)
 				}
+
+				pvcUsage.WithLabelValues(path).Set(usage)
+				log.Printf("Path: %s, Usage: %f bytes", path, usage)
+
+				totalUsage += usage
 
 				return nil
 			})
 			if err != nil {
 				log.Printf("Error walking the path %s: %v", baseDir, err)
 			}
+
+			pvcUsage.WithLabelValues(baseDir).Set(totalUsage)
+			log.Printf("Total Usage for %s: %f bytes", baseDir, totalUsage)
 
 			time.Sleep(10 * time.Second)
 		}
